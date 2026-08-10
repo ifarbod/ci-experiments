59b922bc913d4138fd2d61cf862bf198b4717486
iFarbod <ifarbod@users.noreply.github.com>
2026-08-11 02:11:58 +0330

    Change C++ standard to cxx26 and enhance Clang support
    Updated C++ standard version and added a fallback scanner for Apple Clang.

--- xmake.lua
+++ xmake.lua
@@ -1,19 +1,27 @@
 add_rules("mode.debug", "mode.release")
 
 -- Disable C++ standard library modules build policy
-set_policy("build.c++.modules", true)
+-- set_policy("build.c++.modules", true)
 set_policy("build.c++.modules.std", false)
 
 set_policy("build.c++.modules.hide_dependencies", true)
 
 target("cpp20_modules_app")
     set_kind("binary")
-    set_languages("cxx20")
+    set_languages("cxx26")
+
+    on_load(function (target)
+        local cc = target:tool("cc") or "clang"
+        local ok, outdata = os.iorunv(cc, {"--version"})
+        if ok and outdata and outdata:find("Apple clang", 1, true) then
+            -- xcode doesn't ship clang-scan-deps sadly
+            target:set("policy", "build.c++.modules.clang.fallbackscanner", true)
+        end
+    end)
 
     -- Disable RTTI and Exceptions
     add_cxxflags("-fno-rtti", "-fno-exceptions")
     add_cxxflags("-fcxx-modules")
-    -- add_cxxflags("-fmodules")
 
     add_files("module.cpp")
     add_files("*.cppm")
