6c9f6cf7277bb98efe2eeb323fa09198f62a2877
iFarbod <ifarbod@users.noreply.github.com>
2026-08-11 02:31:30 +0330

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -10,17 +10,8 @@ target("cpp20_modules_app")
     set_kind("binary")
     set_languages("cxx26")
 
-    on_config(function (target)
-        local cc = target:tool("cc")
-        if cc and cc:find("Xcode.app", 1, true) then
-            target:set("policy", "build.c++.modules.clang.fallbackscanner", true)
-            print("using fallback xmake scanner, second method")
-        end
-    end)
-
-    -- Disable RTTI and Exceptions
     add_cxxflags("-fno-rtti", "-fno-exceptions")
-    add_cxxflags("-fcxx-modules")
+    add_cxxflags("-fcxx-modules") -- needed for xcode clang only
 
     add_files("module.cpp")
     add_files("*.cppm")
