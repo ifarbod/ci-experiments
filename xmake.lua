da1acecaee8b74a6a3e4b8a1578d83e61160810b
iFarbod <ifarbod@users.noreply.github.com>
2026-08-11 01:44:21 +0330

    Change language standard and update file inclusion
    Updated language standard from C++26 to C++20 and modified file inclusion for modules.

--- xmake.lua
+++ xmake.lua
@@ -8,10 +8,10 @@ set_policy("build.c++.modules.hide_dependencies", true)
 
 target("cpp20_modules_app")
     set_kind("binary")
-    set_languages("cxx26")
+    set_languages("cxx20")
 
     -- Disable RTTI and Exceptions
     add_cxxflags("-fno-rtti", "-fno-exceptions")
 
-    -- Source files
-    add_files("*.cppm", "module.cpp")
+    add_files("module.cpp")
+    add_files("*.cppm", {cxxflags = {"-fcxx-modules", "-fmodules"}})
