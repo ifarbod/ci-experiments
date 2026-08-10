493b4f29cc642b89e41727580cddfa35d9ea8497
iFarbod <ifarbod@users.noreply.github.com>
2026-08-11 02:21:43 +0330

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -11,22 +11,12 @@ target("cpp20_modules_app")
     set_languages("cxx26")
 
     on_config(function (target)
-        local cc = target:tool("cc") or "clang"
-        local ok, outdata = os.iorunv(cc, {"--version"})
-        if ok and outdata and outdata:find("Apple clang", 1, true) then
-            -- xcode doesn't ship clang-scan-deps sadly
+        local cc = target:tool("cc")
+        if cc and cc:find("Xcode.app", 1, true) then
             target:set("policy", "build.c++.modules.clang.fallbackscanner", true)
-            print("using fallback xmake scanner")
+            print("using fallback xmake scanner, second method")
         end
     end)
-
-    -- on_config(function (target)
-    --     local cc = target:tool("cc")
-    --     if cc and cc:find("Xcode.app", 1, true) then
-    --         target:set("policy", "build.c++.modules.clang.fallbackscanner", true)
-    --         print("using fallback xmake scanner, second method")
-    --     end
-    -- end)
 
     -- Disable RTTI and Exceptions
     add_cxxflags("-fno-rtti", "-fno-exceptions")
