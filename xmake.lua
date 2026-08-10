a74bc0842427134d91f92497562e83449c1ed35c
iFarbod <ifarbod@users.noreply.github.com>
2026-08-11 01:15:21 +0330

    Create xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+add_rules("mode.debug", "mode.release")
+
+-- Disable C++ standard library modules build policy
+set_policy("build.c++.modules.std", false)
+
+target("cpp20_modules_app")
+    set_kind("binary")
+    set_languages("c++20")
+
+    -- Disable RTTI and Exceptions
+    add_rflags("-fno-rtti", "-fno-exceptions")
+    add_cxxflags("-fno-rtti", "-fno-exceptions")
+
+    -- Source files
+    add_files("*.cppm", "module.cpp")
